267204a9ac94bea51d79950f191dcf3ba84e7b29
merdie <merdiemlb@gmail.com>
2021-11-19 16:34:56 +0100

    feat: add router.push

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,14 @@
 import Link from "next/link";
+import { useRouter } from "next/router";
 
 const Home = () => {
+  const router = useRouter();
+  const handleClick = () => {
+    console.log("router before push", router);
+    console.log("Placing your order");
+    router.push("/products");
+    console.log("router after push", router);
+  };
   return (
     <div>
       <h1>Home</h1>
@@ -10,6 +18,7 @@ const Home = () => {
       <Link href="/products">
         <a>Go to Products</a>
       </Link>
+      <button onClick={handleClick}>Place an order</button>
     </div>
   );
 };
